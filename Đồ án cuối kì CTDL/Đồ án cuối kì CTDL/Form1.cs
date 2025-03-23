91237af54cce2fe1f54a1db7f6a0866101a85523
ThanhVu165 <Ndthanhvu@gmail.com>
2025-03-23 16:39:01 +0700

    Add files via upload

---
+++ Đồ án cuối kì CTDL/Đồ án cuối kì CTDL/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ArtanButton1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Runtime.InteropServices;
+namespace Đồ_án_cuối_kì_CTDL
+{
+    
+
+    public partial class Form1 : Form
+    {
+        
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        private const int WM_NCLBUTTONDOWN = 0xA1; // Mã sự kiện kéo cửa sổ
+        private const int HTCAPTION = 0x2;        // Mã thanh tiêu đề của cửa sổ
+
+
+        string result = "";
+        string Ans = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Giúp di chuyển window
+        private void TitlePanel_Paint(object sender, PaintEventArgs e)
+        {
+            TitlePanel.MouseDown += PanelTitleBar_MouseDown;
+        }
+        private void PanelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+
+
+        //Nút tắt
+        private void ExitBtn_Click(object sender, EventArgs e)   
+        {
+            Close();
+        }
+        //Nút thu nhỏ min
+        private void MiniBtn_Click(object sender, EventArgs e)   
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+
+        //set cái cursor về bên lề phải để số được in ra đúng từ lề
+        
+        private void TextDisplay2_TextChanged(object sender, EventArgs e)
+        {
+            UpdateCursorPosition2();
+        }
+        private void UpdateCursorPosition2()
+        {
+            TextDisplay2.SelectionStart = TextDisplay2.TextLength;
+            TextDisplay2.ScrollToCaret();
+        }
+
+
+
+
+
+        //Nút số 
+        private void Num_Click(object sender, EventArgs e)
+        {
+            ArtanButton button = new ArtanButton();
+            button = (ArtanButton)sender;
+            TextDisplay2.Text = TextDisplay2.Text + button.Text;
+            UpdateCursorPosition2();
+
+        }
+        //Nút dấu đơn giản
+        private void MathOperators_Click(object sender, EventArgs e)
+        {
+            ArtanButton button = new ArtanButton();
+            button = (ArtanButton)sender;
+            TextDisplay2.Text = TextDisplay2.Text + button.Text;
+            
+            UpdateCursorPosition2();
+        }
+        //Nút dấu nâng cao
+        private void SpecOperators_Click(object sender, EventArgs e)
+        {
+            ArtanButton button = new ArtanButton();
+            button = (ArtanButton)sender;
+            if (button.Text == "1/x")
+            {
+                TextDisplay2.Text = TextDisplay2.Text + "1/";
+            }
+            if (button.Text == "√x")
+            {
+                TextDisplay2.Text = TextDisplay2.Text + "√";
+            }
+            UpdateCursorPosition2();
+        }
+
+
+
+        //Xóa dữ liệu đầu vào 
+        private void ClearEntryBtn_Click(object sender, EventArgs e)
+        {
+            TextDisplay2.Text = null;
+        }
+        //Xóa toàn bộ dữ liệu trên màn hình hiển thị
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            TextDisplay1.Text = null;
+            TextDisplay2.Text = null;
+        }
+        //Xóa từ phải sang trái
+        private void Backspace_Click(object sender, EventArgs e)
+        {
+            if (TextDisplay2.TextLength > 0)
+            {
+                if (TextDisplay2.Text.Substring(TextDisplay2.TextLength - 1, 1) == "s")
+                {
+                    TextDisplay2.Text = TextDisplay2.Text.Substring(0, TextDisplay2.TextLength - 3);
+                }
+                else if (TextDisplay2.Text.Substring(TextDisplay2.TextLength - 1, 1) == "/")
+                {
+                    TextDisplay2.Text = TextDisplay2.Text.Substring(0, TextDisplay2.TextLength - 2);
+                }
+                else
+                    TextDisplay2.Text = TextDisplay2.Text.Substring(0, TextDisplay2.TextLength - 1);
+            } 
+        }
+       
+
+        private void AnsBtn_Click(object sender, EventArgs e)
+        {
+            TextDisplay2.Text = TextDisplay2.Text + "Ans";
+        }
+
+
+
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            result = TextDisplay2.Text;
+            TextDisplay1.Text = TextDisplay2.Text;
+            Solve solve = new Solve();  
+            solve.Input(result);
+            string memaybeo = solve.GetResult().ToString();
+            TextDisplay2.Text = memaybeo;
+        }
+        
+
+        
+    }
+}
---
+++ Đồ án cuối kì CTDL/Đồ án cuối kì CTDL/Solve.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Đồ_án_cuối_kì_CTDL
+{
+    public class Solve
+    {
+        
+        private string Infix;
+
+        public void Input(string input)
+        {
+            Infix = input;
+        }
+
+        public double GetResult()
+        {
+            double result = 0;
+            string Postfix = ConvertInfix(Infix);
+            result = TinhPostfix(Postfix);
+
+            return result;
+        }
+        public bool Operators(char value)
+        {
+            return value == '＋' || value == '－' || value == '×' || value == '÷' || value == '^' || value == '√' || value == '/';
+        }
+
+        static int Priority(char value)
+        {
+            switch (value)
+            {
+                case '＋':
+                case '－':
+                    return 1;
+                case '×':
+                case '/':
+                case '÷':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+        public string ConvertInfix(string Infix)
+        {
+            MyStack myStack = new MyStack();
+            string Postfix = "";
+
+            foreach (char value in Infix)
+            {
+                if (char.IsDigit(value))
+                {
+                    Postfix += value;
+                }
+                else if (Operators(value))
+                {
+                    while (!myStack.IsEmpty() && Priority((char)myStack.Peek()) >= Priority(value))
+                    {
+                        Postfix += myStack.Pop();
+                    }
+                    myStack.Push(value);
+                }
+                else if (value == '(')
+                {
+                    myStack.Push(value);
+                }
+                else if (value == ')')
+                {
+                    while (!myStack.IsEmpty() && (char)myStack.Peek() != '(')
+                    {
+                        Postfix += myStack.Pop();
+                    }
+                    if (myStack.Count() > 0 && (char)myStack.Peek() == '(')
+                    {
+                        myStack.Pop();
+                    }
+                }
+            }
+            while (myStack.Count() > 0)
+            {
+                Postfix += myStack.Pop();
+            }
+            return Postfix;
+        }
+
+
+
+        public double TinhPostfix(string Postfix)
+        {
+            MyStack myStack = new MyStack();
+
+            foreach (char value in Postfix)
+            {
+
+                if (char.IsDigit(value))
+                {
+                    myStack.Push((double)(value - '0'));
+                }
+                else
+                {
+                    double Num2 = Convert.ToDouble(myStack.Pop());
+                    double Num1 = Convert.ToDouble(myStack.Pop());
+
+                    switch (value)
+                    {
+                        case '＋':
+                            myStack.Push((double)Num1 + Num2);
+                            break;
+                        case '－':
+                            myStack.Push((double)Num1 - Num2);
+                            break;
+                        case '×':
+                            myStack.Push((double)Num1 * Num2);
+                            break;
+                        case '÷':
+                        case '/':
+                            myStack.Push((double)Num1 / Num2);
+                            break;
+                        case '^':
+                            myStack.Push((double)Math.Pow(Num1, Num2));
+                            break;
+                    }
+                }
+            }
+
+            return Convert.ToDouble(myStack.Pop());
+        }
+    }
+}
